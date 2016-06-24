eeb905da73a6efa383f856c9806e6a2ce8026ffc
Sun Ziping <sunziping@gmail.com>
2016-06-24 15:42:16 +0800

    fix tabs

--- lambda.h
+++ lambda.h
@@ -2,7 +2,7 @@
 #define LAMBDA_LAMBDA_H
 
 #include <type_traits>
-    
+
 template<class A1, class A2> void do_nothing(const A1 &, const A2&) {}
 #define CALL_USE_ARGS do_nothing(a, b)  // suppress -Wunused-parameter
 
@@ -14,46 +14,46 @@ class subscript_action;
 template<class T>
 class lambda_functor: public T {
 public:
-	typedef T inherited;
+    typedef T inherited;
 
-	lambda_functor() = default;
-	lambda_functor(const lambda_functor &l): inherited(l) {}
-	lambda_functor(const T &t): inherited(t) {}
+    lambda_functor() = default;
+    lambda_functor(const lambda_functor &l): inherited(l) {}
+    lambda_functor(const T &t): inherited(t) {}
 
-	template<class A>
-	auto operator()(A &a) const -> decltype(inherited::template call(a, nullptr)) {
-		return inherited::template call(a, nullptr);
-	}
-	template<class A>
-	auto operator()(const A &a) const -> decltype(inherited::template call(a, nullptr)) {
-		return inherited::template call(a, nullptr);
-	}
-	template<class A, class B>
-	auto operator()(A &a, B &b) const -> decltype(inherited::template call(a, b)) {
-		return inherited::template call(a, b);
-	}
-	template<class A, class B>
-	auto operator()(A &a, const B &b) const -> decltype(inherited::template call(a, b)) {
-		return inherited::template call(a, b);
-	}
-	template<class A, class B>
-	auto operator()(const A &a, B &b) const -> decltype(inherited::template call(a, b)) {
-		return inherited::template call(a, b);
-	}
-	template<class A, class B>
-	auto operator()(const A &a, const B &b) const -> decltype(inherited::template call(a, b)) {
-		return inherited::template call(a, b);
-	}
-	template<class A>
-	lambda_functor<lambda_binary_functor<assignment_action, const lambda_functor<T>, const A> >
-	operator = (const A &a) const {
+    template<class A>
+    auto operator()(A &a) const -> decltype(inherited::template call(a, nullptr)) {
+        return inherited::template call(a, nullptr);
+    }
+    template<class A>
+    auto operator()(const A &a) const -> decltype(inherited::template call(a, nullptr)) {
+        return inherited::template call(a, nullptr);
+    }
+    template<class A, class B>
+    auto operator()(A &a, B &b) const -> decltype(inherited::template call(a, b)) {
+        return inherited::template call(a, b);
+    }
+    template<class A, class B>
+    auto operator()(A &a, const B &b) const -> decltype(inherited::template call(a, b)) {
+        return inherited::template call(a, b);
+    }
+    template<class A, class B>
+    auto operator()(const A &a, B &b) const -> decltype(inherited::template call(a, b)) {
+        return inherited::template call(a, b);
+    }
+    template<class A, class B>
+    auto operator()(const A &a, const B &b) const -> decltype(inherited::template call(a, b)) {
+        return inherited::template call(a, b);
+    }
+    template<class A>
+    lambda_functor<lambda_binary_functor<assignment_action, const lambda_functor<T>, const A> >
+    operator = (const A &a) const {
         return lambda_binary_functor<assignment_action, const lambda_functor<T>, const A>(*this, a);
-	}
-	template<class A>
-	lambda_functor<lambda_binary_functor<subscript_action, const lambda_functor<T>, const A> >
-	operator [] (const A &a) const {
-		return lambda_binary_functor<subscript_action, const lambda_functor<T>, const A>(*this, a);
-	}
+    }
+    template<class A>
+    lambda_functor<lambda_binary_functor<subscript_action, const lambda_functor<T>, const A> >
+    operator [] (const A &a) const {
+        return lambda_binary_functor<subscript_action, const lambda_functor<T>, const A>(*this, a);
+    }
 };
 
 #define EXPRESSION(EXP)                                                                                             \
@@ -65,7 +65,7 @@ template<class A, class B> auto call(const A &a, const B &b) const -> decltype(E
 template<int I> struct placeholder;
 #define LAMBDA_PLACEHOLDER(POSITION, RETURN_ARGUMENT, NAME) \
 template<> struct placeholder<POSITION> {                   \
-	EXPRESSION((RETURN_ARGUMENT))                           \
+    EXPRESSION((RETURN_ARGUMENT))                           \
 };                                                          \
 const auto NAME = lambda_functor<placeholder<POSITION> >();
 LAMBDA_PLACEHOLDER(0, a, _1)
@@ -74,24 +74,24 @@ LAMBDA_PLACEHOLDER(1, b, _2)
 
 template<class Any, class A, class B>
 inline Any &select(Any &any, const A &a, const B&b) {
-	CALL_USE_ARGS;
-	return any;
+    CALL_USE_ARGS;
+    return any;
 }
 template<class Arg, class A, class B>
 inline auto select(const lambda_functor<Arg> &op, A &a, B &b) -> decltype(op.template call(a, b)) {
-	return op.template call(a, b);
+    return op.template call(a, b);
 }
 template<class Arg, class A, class B>
 inline auto select(const lambda_functor<Arg> &op, const A &a, B &b) -> decltype(op.template call(a, b)) {
-	return op.template call(a, b);
+    return op.template call(a, b);
 }
 template<class Arg, class A, class B>
 inline auto select(const lambda_functor<Arg> &op, A &a, const B &b) -> decltype(op.template call(a, b)) {
-	return op.template call(a, b);
+    return op.template call(a, b);
 }
 template<class Arg, class A, class B>
 inline auto select(const lambda_functor<Arg> &op, const A &a, const B &b) -> decltype(op.template call(a, b)) {
-	return op.template call(a, b);
+    return op.template call(a, b);
 }
 
 template<class Action, class ArgA, class ArgB>
@@ -100,10 +100,10 @@ struct lambda_binary_functor;
 class ACTION_CLASS{};                                                           \
 template<class ArgA, class ArgB>                                                \
 struct lambda_binary_functor<ACTION_CLASS, ArgA, ArgB> {                        \
-	ArgA &arga;                                                                 \
-	ArgB &argb;                                                                 \
-	lambda_binary_functor(ArgA &arga, ArgB &argb): arga(arga), argb(argb) {}    \
-	EXPRESSION(select(arga, a, b) SYMBOL select(argb, a, b))                    \
+    ArgA &arga;                                                                 \
+    ArgB &argb;                                                                 \
+    lambda_binary_functor(ArgA &arga, ArgB &argb): arga(arga), argb(argb) {}    \
+    EXPRESSION(select(arga, a, b) SYMBOL select(argb, a, b))                    \
 };
 #define LAMBDA_BINARY_ACTION_OPERATOR(SYMBOL, ACTION_CLASS)                                                     \
 template<class Arg, class B>                                                                                    \
@@ -132,8 +132,8 @@ operator SYMBOL (const lambda_functor<A> &a, const lambda_functor<B> &b) {      
     return lambda_binary_functor<ACTION_CLASS, const lambda_functor<A>, const lambda_functor<B> >(a, b);        \
 }
 #define LAMBDA_BINARY_ACTION(SYMBOL, ACTION_CLASS)      \
-	LAMBDA_BINARY_ACTION_CLASS(SYMBOL, ACTION_CLASS)    \
-	LAMBDA_BINARY_ACTION_OPERATOR(SYMBOL, ACTION_CLASS)
+    LAMBDA_BINARY_ACTION_CLASS(SYMBOL, ACTION_CLASS)    \
+    LAMBDA_BINARY_ACTION_OPERATOR(SYMBOL, ACTION_CLASS)
 
 template<class Action, class Arg>
 struct lambda_prefix_unary_action{};
@@ -141,9 +141,9 @@ struct lambda_prefix_unary_action{};
 class ACTION_CLASS{};                                                                       \
 template<class Arg>                                                                         \
 struct lambda_prefix_unary_action<ACTION_CLASS, Arg> {                                      \
-	Arg &arg;                                                                               \
-	lambda_prefix_unary_action(Arg &arg): arg(arg) {}                                       \
-	EXPRESSION(SYMBOL select(arg, a, b))                                                    \
+    Arg &arg;                                                                               \
+    lambda_prefix_unary_action(Arg &arg): arg(arg) {}                                       \
+    EXPRESSION(SYMBOL select(arg, a, b))                                                    \
 };                                                                                          \
 template<class A>                                                                           \
 inline lambda_functor<lambda_prefix_unary_action<ACTION_CLASS, const lambda_functor<A> > >  \
@@ -157,9 +157,9 @@ struct lambda_postfix_unary_action{};
 class ACTION_CLASS{};                                                                       \
 template<class Arg>                                                                         \
 struct lambda_postfix_unary_action<ACTION_CLASS, Arg> {                                     \
-	Arg &arg;                                                                               \
-	lambda_postfix_unary_action(Arg &arg): arg(arg) {}                                      \
-	EXPRESSION(select(arg, a, b) SYMBOL)                                                    \
+    Arg &arg;                                                                               \
+    lambda_postfix_unary_action(Arg &arg): arg(arg) {}                                      \
+    EXPRESSION(select(arg, a, b) SYMBOL)                                                    \
 };                                                                                          \
 template<class A>                                                                           \
 inline lambda_functor<lambda_postfix_unary_action<ACTION_CLASS, const lambda_functor<A> > > \
@@ -200,10 +200,10 @@ LAMBDA_BINARY_ACTION(^=,  bitwise_xor_assignment_action)
 class comma_action{};
 template<class ArgA, class ArgB>
 struct lambda_binary_functor<comma_action, ArgA, ArgB> {
-	ArgA &arga;
-	ArgB &argb;
-	lambda_binary_functor(ArgA &arga, ArgB &argb): arga(arga), argb(argb) {}
-	EXPRESSION(select(arga, a, b) COMMA select(argb, a, b))
+    ArgA &arga;
+    ArgB &argb;
+    lambda_binary_functor(ArgA &arga, ArgB &argb): arga(arga), argb(argb) {}
+    EXPRESSION(select(arga, a, b) COMMA select(argb, a, b))
 };
 LAMBDA_BINARY_ACTION_OPERATOR(COMMA, comma_action)
 
@@ -212,10 +212,10 @@ LAMBDA_BINARY_ACTION_CLASS(=, assignment_action)
 class subscript_action{};
 template<class ArgA, class ArgB>
 struct lambda_binary_functor<subscript_action, ArgA, ArgB> {
-	ArgA &arga;
-	ArgB &argb;
-	lambda_binary_functor(ArgA &arga, ArgB &argb): arga(arga), argb(argb) {}
-	EXPRESSION(select(arga, a, b)[select(argb, a, b)])
+    ArgA &arga;
+    ArgB &argb;
+    lambda_binary_functor(ArgA &arga, ArgB &argb): arga(arga), argb(argb) {}
+    EXPRESSION(select(arga, a, b)[select(argb, a, b)])
 };
 
 LAMBDA_PREFIX_UNARY_ACTION(+,  unary_plus_action)
@@ -232,9 +232,9 @@ LAMBDA_POSTFIX_UNARY_ACTION(--, post_decrement_action)
 
 template<class Arg>
 struct lambda_var_action {
-	Arg &arg;
-	lambda_var_action(Arg &arg): arg(arg) {}
-	EXPRESSION(select(arg, a, b))
+    Arg &arg;
+    lambda_var_action(Arg &arg): arg(arg) {}
+    EXPRESSION(select(arg, a, b))
 };
 template<class A>
 inline lambda_functor<lambda_var_action<A> > ref(A &a) { return lambda_var_action<A>(a); }
